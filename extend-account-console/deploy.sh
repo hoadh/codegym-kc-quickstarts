473a8f306445ddf9105a17c473bc048ffb533949
hoadh <danghuyhoa@gmail.com>
2021-11-11 15:45:39 +0700

    Copy to production repo

--- extend-account-console/deploy.sh
+++ extend-account-console/deploy.sh
@@ -1,3 +1,7 @@
 mvn clean install
 cp ./target/codegym-keycloak-account-v2.jar /Users/hoa/Workspace/Keycloak/my_sso/
 cp -rf src/main/resources/theme/codegym-theme/account/ /Users/hoa/Workspace/Keycloak/my_sso/themes/codegym-theme/account/
+
+# Copy to production repo
+cp ./target/codegym-keycloak-account-v2.jar /Users/hoa/Workspace/Keycloak/codegym-sso/
+cp -rf src/main/resources/theme/codegym-theme/account/ /Users/hoa/Workspace/Keycloak/codegym-sso/themes/codegym-theme/account/

a0adb371957523751a6aab0235a8f269a8cffac9
hoadh <danghuyhoa@gmail.com>
2021-11-11 15:11:39 +0700

    Add deploy.sh script

---
+++ extend-account-console/deploy.sh
@@ -0,0 +1,3 @@
+mvn clean install
+cp ./target/codegym-keycloak-account-v2.jar /Users/hoa/Workspace/Keycloak/my_sso/
+cp -rf src/main/resources/theme/codegym-theme/account/ /Users/hoa/Workspace/Keycloak/my_sso/themes/codegym-theme/account/
